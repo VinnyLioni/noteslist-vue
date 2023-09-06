6a097e62e32d8dca51dab46f123032c8f9d65e50
VinnyLioni <vinnylioni@gmail.com>
2023-09-06 10:13:51 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <RouterLink :to="{name: 'notes'}">Notes</RouterLink> |
+      <RouterLink :to="{name: 'stats'}">Stats</RouterLink>
+    </div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
